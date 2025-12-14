353e558b9ba0abfa7229bfe1a17a73dc928c48a5
anunanda-s20 <anunandasjeeva@gmail.com>
2025-12-14 17:05:55 +0530

    Improve Book admin with custom display, search, filter

--- book_app/admin.py
+++ book_app/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Book
 
-admin.site.register(Book)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'published_date')
+    search_fields = ('title', 'author')
+    list_filter = ('published_date',)
+    ordering = ('-published_date',)
