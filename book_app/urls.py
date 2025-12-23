b9bcbe05623c6235e011db719361502c3cd75931
anunanda-s20 <anunandasjeeva@gmail.com>
2025-12-23 20:58:28 +0530

    Book list page completed

--- book_app/urls.py
+++ book_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('books/', views.book_list, name='book_list'),
+    path('books/<int:id>/', views.book_detail, name='book_detail'),
 ]
--- book_app/views.py
+++ book_app/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Book
 
 # Create your views here.
 def home(request):
     return render(request, 'book_app/home.html')
+
+
+
+def book_list(request):
+    books = Book.objects.all()  # Get all books
+    return render(request, 'book_app/book_list.html', {'books': books})
+
+def book_detail(request, id):
+    book = get_object_or_404(Book, id=id)
+    return render(request, 'book_app/book_detail.html', {'book': book})
