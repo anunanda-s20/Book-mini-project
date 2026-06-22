75ed292a3fc97f1a8ba7bcb87a48e8c0ae9f5701
anunanda-s20 <anunandasjeeva@gmail.com>
2026-06-22 17:31:08 +0530

    Prepare project for deployment

--- book_project/settings.py
+++ book_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-%v+$^=u=%8_cym@j=2n3h$5ywjtk=gz9k_(clkre3rn@1s13o"
 
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # ================= APPLICATIONS =================
@@ -109,7 +109,7 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "book_app" / "static",  # -  for static files
 ]
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # ================= MEDIA FILES =================
 MEDIA_URL = "/media/"
