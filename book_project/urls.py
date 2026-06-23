df1b9ae8d424db6d70abc7aa494bb1b1652a4256
anunanda-s20 <anunandasjeeva@gmail.com>
2026-06-23 21:13:59 +0530

    urls.py : Added MEDIA static config in project urls.py to fix image loading issue

--- book_project/urls.py
+++ book_project/urls.py
@@ -19,6 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views  # built-in authentication views (login, password reset)
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     
     path("admin/", admin.site.urls),  
@@ -43,3 +46,6 @@ urlpatterns = [
     path('reset_done/',auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),name='password_reset_complete'),
     # final success page after password change
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
