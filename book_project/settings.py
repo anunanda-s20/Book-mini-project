b1d329fa59aca6220d3ca85daff0a1a5b5334b81
anunanda-s20 <anunandasjeeva@gmail.com>
2026-06-23 18:00:41 +0530

    settings.py : Configure WhiteNoise for production static files

--- book_project/settings.py
+++ book_project/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
 # ================= MIDDLEWARE =================
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -129,6 +131,9 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = (
+    "whitenoise.storage.CompressedManifestStaticFilesStorage"
+)
 
 
 
