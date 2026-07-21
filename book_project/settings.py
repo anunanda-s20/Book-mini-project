b5af37f4b63c0ef31272e5eb357dcb6f88ef7846
anunanda-s20 <anunandasjeeva@gmail.com>
2026-07-21 16:27:45 +0530

    security: move SMTP credentials to environment variables
    Replace hardcoded Gmail SMTP credentials with environment variables for secure production configuration.

--- book_project/settings.py
+++ book_project/settings.py
@@ -146,10 +146,6 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# STATICFILES_STORAGE = (
-#     "whitenoise.storage.CompressedManifestStaticFilesStorage"
-# )
-# Compresses static files and improves production performance
 
 
 # ================= MEDIA FILES =================
@@ -170,8 +166,11 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'anunandasjeeva@gmail.com'
-EMAIL_HOST_PASSWORD = 'yivlfgsixquycuyj'
+'''EMAIL_HOST_USER = 'anunandasjeeva@gmail.com'
+EMAIL_HOST_PASSWORD = 'yivlfgsixquycuyj'''
+
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
